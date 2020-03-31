6682fa976445729529618686ef7d5408c8ea39d3
ColorsWind <1748591300@qq.com>
2020-03-31 22:13:33 +0800

    修改页面标题

--- src/net/colors_wind/submitmanager/MainWindow.java
+++ src/net/colors_wind/submitmanager/MainWindow.java
@@ -43,6 +43,7 @@ public class MainWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public MainWindow() {
+		setTitle("作业管理器");
 		setType(Type.POPUP);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 580, 433);
